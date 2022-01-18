f821ffaf2d7bf0ae0a6b6d6e77d48c45353fbaec
as2025 <as2025@hw.ac.uk>
2022-01-18 17:24:29 +0400

    Moved directories

--- Backend/BackendMain/views.py
+++ Backend/BackendMain/views.py
@@ -7,7 +7,7 @@ def getBack(request):
     uri = "mongodb+srv://zinasktest.uxarp.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
     client = pymongo.MongoClient(uri,
                         tls=True,
-                        tlsCertificateKeyFile='BackendMain/X509-cert-7626596970333998897.pem')
+                        tlsCertificateKeyFile='Backend/BackendMain/X509-cert-7626596970333998897.pem')
 
     db = client['testDB']
     collection = db['testCol']
