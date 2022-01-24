b9e13e4815ec86ab33d30457f87a035b49f3f4ef
na2007 <na2007@hw.ac.uk>
2022-01-24 22:42:05 +0400

    completed the folders

--- src/Componenets/Workspace/Workspace.jsx
+++ src/Componenets/Workspace/Workspace.jsx
@@ -1,9 +1,156 @@
-import React from 'react';
-import Topbar from '../Topbar/Topbar';
+// import React, { Component } from "react";
+// // import axios, { Axios } from 'axios'; 
 
+
+// class Upload extends Component {
+//     state = {
+//         selectedFile: null
+//     }
+//     fileSelect = event => {
+//         this.setState({
+//             selectedFile: event.target.files[0]
+//         })
+//         console.log(event.target.files[0]);
+//        }
+//     fileUpload = () => {
+//         const fd = new FormData();
+//         fd.append('image',this.state.selectedFile, this.state.selectedFile.name);
+//         // Axios.post('', fd);
+//     }
+
+//     render() {
+//         return (
+//             <div className="Upload">
+//                 <input type="file" onChange={this.fileSelect}/>
+//                 <button onClick={this.fileUpload}>Upload</button>
+//             </div>
+//         )
+//     }
+    
+// }
+
+// export default Upload;
+    
+import React from 'react';
+import {FaSearch,FaHome} from 'react-icons/fa'
+
+import { Link } from "react-router-dom";
+import {FaBackspace,FaHistory } from 'react-icons/fa';
+
+import { MdSettings,MdAddBox} from "react-icons/md"
+import {GoGraph} from "react-icons/go"
+
+import {AiFillEdit,AiFillFolderAdd } from "react-icons/ai";
+import { RiTeamFill } from "react-icons/ri";
+import "./Workspace.css"
+import {RiChatSmile2Fill} from  "react-icons/ri"
+import { BsImageFill } from "react-icons/bs";
 function Workspace() {
   return <div>
-      <Topbar/>
+       <div className="navbar-container">
+        <div className="navbar-left">
+        {/* <Link to='#' className='menu-bars'>
+            <FaBars onClick={showSidebar} />
+          </Link> */}
+          <span className="navbar-logo"><Link to="/home">Canary</Link></span>
+        </div>
+        <div className="navbar-center">
+          <div className="navbar-search">
+          <FaSearch className="search"/>
+            <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+          </div>
+        </div>
+        <div className="navbar-right">
+          <div className="navbar-icons">
+          <div className="navbar-IconItem">
+      <Link to="/"><span className='project-text'>Project Name</span></Link>
+          </div>
+          <div className="navbar-IconItem">
+         
+          <Link to="/"><span className='project-text' >Save</span></Link>
+          </div>
+          <div className="navbar-IconItem">
+          <Link to="/"><span className='project-text'>Share</span></Link>
+          </div>
+         
+          </div>
+        </div>
+      
+      </div>
+      <div className="sidenav">
+                <div className="sidenav-container">
+            
+                    <ul className="sidenav-list">
+                        <li className="sidenav-item">
+                        <FaBackspace className="sidenav-project"/>
+                        <Link to="/projects">
+                        <span className="sidenav-text">Back</span> 
+                            </Link>
+                        </li>
+                       <li className="sidenav-item">
+                        <AiFillEdit className="sidenav-project"/>
+                        <Link to="/home">
+                          <span className="sidenav-text">Editor</span> 
+                        </Link>
+                        </li>
+                      <li className="sidenav-item">
+                        <RiTeamFill className="sidenav-project"/>
+                        <Link to="/home">
+                             <span className="sidenav-text">Team</span> 
+                            </Link>
+                        </li>
+                       <li className="sidenav-item">
+                        <RiChatSmile2Fill  className="sidenav-project"/>
+                        <Link to="/">
+                             <span className="sidenav-text">Chat</span> 
+                        </Link>
+                        </li>
+                        <li className="sidenav-item">
+                        <FaHistory  className="sidenav-project"/>
+                        <Link to="/">
+                             <span className="sidenav-text">History</span> 
+                        </Link>
+                        </li>
+
+                        <li className="sidenav-item">
+                        <GoGraph  className="sidenav-project"/>
+                        <Link to="/">
+                             <span className="sidenav-text">Satistics</span> 
+                        </Link>
+                        </li>
+                        <li className="sidenav-item">
+                        <MdSettings  className="sidenav-project"/>
+                        <Link to="/">
+                             <span className="sidenav-text">Settings</span> 
+                        </Link>
+                        </li>
+                      
+                    </ul>
+                </div> 
+          
+           </div>
+        <div className="folder">
+        <div className="folders">
+        <BsImageFill className='folder-icon'/>
+        <h3 className='folder-text'>Folder 1</h3>
+        
+        </div>
+        <div className="folders">
+        <BsImageFill className='folder-icon'/>
+        <h3 className='folder-text'>Folder 2</h3>
+        </div>
+        <div className="folders">
+        <BsImageFill className='folder-icon'/>
+        <h3 className='folder-text'>Folder 3</h3>
+        </div>
+        <div className="folders">
+        <MdAddBox className='folder-icon new'/>
+        </div>
+        </div>
+          
   </div>;
 }
 
