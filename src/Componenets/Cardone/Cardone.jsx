899cc2e1421a87cd1c60a60a375643d85a813867
im90866 <im90866@gmail.com>
2022-01-26 17:29:50 +0400

    CRUD functionality (projects)
    Working CRUD functionality for the projects page.
    CardOne and CardTwo, will be deleted in the next push, since they are combined in the proejct.jsx file.
    
    Issue: adding the members as objects

--- src/Componenets/Cardone/Cardone.jsx
+++ src/Componenets/Cardone/Cardone.jsx
@@ -1,38 +1,57 @@
 import "./Cardone.css"
+import api from '../../API/projects'
+import React, { useState, useEffect } from 'react';
+import { BiEdit } from "react-icons/bi";
+import { BsFillTrashFill } from "react-icons/bs";
+import Cardtwo from "../Cardtwo/Cardtwo";
 
-import React from 'react';
-import {BiEdit} from "react-icons/bi";
-import {BsFillTrashFill} from "react-icons/bs";
-function Cardone() {
-  return <div>
-      <div className="project">
-          <div className="existing-projects">
-              <span className="title">Existing Projects</span>
-              <ul className="projectlist">
-                  <div className="projectlistname">
-                  <li >Project1</li>
-                  <BiEdit className="project-icon"/>
-                  <BsFillTrashFill className="project-delete"/>
-                  </div>
-                  <div className="projectlistname">
-                  <li >Project2</li>
-                  <BiEdit className="project-icon"/>
-                  <BsFillTrashFill className="project-delete"/>
-                  </div>
-                  <div className="projectlistname">
-                  <li >Project3</li>
-                  <BiEdit className="project-icon"/>
-                  <BsFillTrashFill className="project-delete"/>
-                  </div>
-                  <div className="projectlistname">
-                  <li >Project4</li>
-                  <BiEdit className="project-icon"/>
-                  <BsFillTrashFill className="project-delete"/>
-                  </div>
-              </ul>
-          </div>
-      </div>
-  </div>;
+function Cardone({}) {
+
+    const [projects, setProjects] = useState([])
+    const getProjects = async () => {
+        const response = await api.get("/allProjects")
+        return response.data
+    }
+
+    useEffect(() => {
+        const getAll = async () => {
+            const allProjects = await getProjects()
+            if (allProjects)
+            setProjects(allProjects)
+        }
+        getAll();
+    }, [])
+
+    const removeProject = async(id) =>{
+        await api.delete(`/allProjects/${id}`)
+        const newProjectList = projects.filter((project) =>{
+            return project.id !== id
+        })
+        setProjects(newProjectList)
+    }
+
+    const editProject = (id) =>{
+        
+    }
+
+    return <div>
+        <div className="project">
+            <div className="existing-projects">
+                <span className="title">Existing Projects</span>
+                <ul className="projectlist">
+                    {
+                        projects.map(project =>
+                            <div className="projectlistname" key={project.id}>
+                                <li >{project.projectName}</li>
+                                <BiEdit className="project-icon" onClick={() => editProject(project.id)}/>
+                                <BsFillTrashFill className="project-delete" onClick={() => removeProject(project.id)}/>
+                            </div>
+                        )
+                    }
+                </ul>
+            </div>
+        </div>
+    </div>;
 }
 
 export default Cardone;
--- src/Componenets/Projects/Projects.jsx
+++ src/Componenets/Projects/Projects.jsx
@@ -2,13 +2,185 @@ import React from 'react';
 import Cardone from "../Cardone/Cardone";
 import Cardtwo from "../Cardtwo/Cardtwo";
 import Topbar from '../Topbar/Topbar';
+import api from '../../API/projects'
+import { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { BiEdit } from "react-icons/bi";
+import { BsFillTrashFill } from "react-icons/bs";
+
 function Projects() {
-  return (<div>
-      <Topbar/> 
-  <div className="project-container">
-      <Cardone/>
-      <Cardtwo/>
-  </div>
+
+  const [projects, setProjects] = useState([])
+  const[update, setUpdate] = useState(false)
+
+  const getProjects = async () => {
+    const response = await api.get("/allProjects")
+    return response.data
+  }
+
+  useEffect(() => {
+    const getAll = async () => {
+      const allProjects = await getProjects()
+      if (allProjects)
+        setProjects(allProjects)
+    }
+    getAll();
+  }, [])
+
+  const [id, setId] = useState("")
+  const [projectName, setProjectName] = useState("")
+  const [type, setType] = useState("")
+  const [members, setMembers] = useState({
+    member1: "",
+    member2: "",
+    member3: "",
+    member4: ""
+  })
+
+  const addProject = async (project) => {
+    console.log(project)
+    const request = {
+      id: uuidv4(),
+      ...project
+    }
+
+    const response = await api.post("/allProjects", request)
+    console.log(response)
+    // setProjects([...project, response.data])
+
+    const getAll = async () => {
+      const allProjects = await getProjects()
+      setProjects(allProjects)
+    }
+    getAll();
+  }
+
+  const removeProject = async (id) => {
+    await api.delete(`/allProjects/${id}`)
+    const newProjectList = projects.filter((project) => {
+      return project.id !== id
+    })
+    setProjects(newProjectList)
+  }
+
+  const editProject = async(id) => {
+    const response = await api.get(`/allProjects/${id}`)
+    const {projectName, type, members} = response.data
+    setId(id)
+    setProjectName(projectName)
+    setType(type)
+    setMembers(members)
+    setUpdate(true)
+  }
+
+  const updateProject = async (project) =>{
+    const response = await api.put(`/allProjects/${project.id}`, project)
+    const {id, projectName, type, members} = response.data
+    setProjects(
+      projects.map((project) =>{
+        return project.id === id ? {...response.data} : project
+      })
+    )
+    setId("")
+    setProjectName("")
+    setType("")
+    setMembers({
+      member1: "",
+      member2: "",
+      member3: "",
+      member4: ""
+    })
+    setUpdate(false)
+  }
+
+  return (
+    <div>
+      <Topbar />
+      <div className="project-container">
+        {/* <Cardone />
+        <Cardtwo /> */}
+
+
+        <div className="project">
+          <div className="existing-projects">
+            <span className="title">Existing Projects</span>
+            <ul className="projectlist">
+              {
+                projects.map(project =>
+                  <div className="projectlistname" key={project.id}>
+                    <li >{project.projectName}</li>
+                    <BiEdit className="project-icon" onClick={() => editProject(project.id)} />
+                    <BsFillTrashFill className="project-delete" onClick={() => removeProject(project.id)} />
+                  </div>
+                )
+              }
+            </ul>
+          </div>
+        </div>
+
+        <div className="create-project">
+          {/* <form onSubmit={add}> */}
+          <div className="create-projecttitle">
+            <span class="cptitle">Create Project</span>
+          </div>
+          <input type="text" value={id} hidden/>
+          <div className="project-form">
+            <input type="text"
+              className='field'
+              placeholder="Project Name"
+              name="Project Name"
+              value={projectName}
+              onChange={event => setProjectName(event.target.value)}
+            /><br></br>
+            <label for="project-type " className='type'>Project Type</label>
+            <select id="project-type" name="project-type" className='project-type' value={type} onChange={event => setType(event.target.value)}>
+              <option value="individual">Individual Project</option>
+              <option value="group">Group Project</option>
+            </select><br></br>
+            <br></br>
+            <label for="members-names" className='members'>Names of the members</label><br></br>
+
+            <input type="text"
+              className='field'
+              placeholder="Member Name"
+              name=" Member Name"
+              value={members.member1}
+              onChange={event => setMembers(event.target.value)}
+            />
+            <input type="text"
+              className='field'
+              placeholder="Member Name"
+              name="Member Name"
+              value={members.member2}
+              onChange={event => setMembers(event.target.value)}
+            />
+            <input type="text"
+              className='field'
+              placeholder="Member Name"
+              name="Member Name"
+              value={members.member3}
+              onChange={event => setMembers(event.target.value)}
+            />
+            <input type="text"
+              className='field'
+              placeholder="Member Name"
+              name="Member Name"
+              value={members.member4}
+              onChange={event => setMembers(event.target.value)}
+            />
+          </div>
+          {
+            update !== true
+            ?
+            <button className='hero-btn' onClick={() => addProject({ projectName, type, members })}>Create project</button>
+            :
+            <button className='hero-btn' onClick={() => updateProject({ id, projectName, type, members })}>Update project</button>
+          }
+          
+          {/* </form> */}
+        </div>
+
+      </div>
     </div>
 
   );
