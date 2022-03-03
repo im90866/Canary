27e9d1d0838428d375a139fea4f1246de1561c76
im90866 <im90866@gmail.com>
2022-03-03 13:50:42 +0400

    CSS fix with search (incomplete)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { Route, Routes, useNavigate,Navigate} from 'react-router-dom';
+import { Route, Routes, useNavigate, Navigate } from 'react-router-dom';
 import './App.css';
-import { useEffect, useState} from "react"
+import { useEffect, useState } from "react"
 
 import Home from './newcomponents/Home/Home';
 import Team from './newcomponents/Team/Team'
@@ -24,6 +24,8 @@ import Settingsbar from './newcomponents/Settings/Settingsbar';
 import Cardthree from './newcomponents/Settings/Cardthree';
 import Cardfour from './newcomponents/Settings/Cardfour';
 import Cardfive from './newcomponents/Settings/Cardfive';
+import Topbar from './newcomponents/Topbar/Topbar'
+import Sidebar from './newcomponents/Sidebar/Sidebar'
 
 function App() {
   const [logged, setLogged] = useState(false)
@@ -34,7 +36,7 @@ function App() {
     console.log("change")
     setLogged(getCookie('username') != null)
 
-    if(getCookie('username') != null)
+    if (getCookie('username') != null)
       navigate('/home')
 
     //axios.get("http://localhost:8000/delete")
@@ -42,54 +44,58 @@ function App() {
   }, [logged])
 
   const protectOther = (component) => {
-    if(getCookie('username') != null) {
+    if (getCookie('username') != null) {
       console.log("The answer is: " + getCookie('username') != null)
       return component
     }
     else {
       console.log("sus")
-      return(<Navigate to="/"/>)
+      return (<Navigate to="/" />)
     }
   }
 
   const protectLogin = () => {
-    if(logged)
-      return(<Navigate to="/home"/>)
+    if (logged)
+      return (<Navigate to="/home" />)
     else
-      return(<Overlay logger={setLogged} check={logged}/>)
+      return (<Overlay logger={setLogged} check={logged} />)
   }
 
-  return (  
+  return (
     <>
-    <CSRFToken />
-    <Routes>
+      <Topbar />
+      <Sidebar />
+      <CSRFToken />
+      <Routes>
 
-      <Route path="/" element={protectLogin()}/>
+        <Route path="/" element={protectLogin()} />
 
-      
-      <Route path="/home" element={protectOther(<Home />)}/>
 
-      {/* <Route path="/signup" element={<Signup />}></Route> */}
-      {/* <Route path="/mainspace" element={<Mainspace />}></Route>
+        <Route path="/home" element={protectOther(<Home />)} />
+
+        {/* <Route path="/signup" element={<Signup />}></Route> */}
+        {/* <Route path="/mainspace" element={<Mainspace />}></Route>
       <Route path="/projects" element={<Projects />}></Route> */}
 
-      <Route path="/workspace/:id" element={<Workspace />}/>
+        <Route path="/workspace/:id" element={<Workspace />} />
 
-      <Route path="/profile" element={<Profile />}/>
-      <Route path="/profile/collaborations" element={<Collaboration/>}/>
-      <Route path="/profile/profileposts" element={<Profileposts/>}/>
-      <Route path="/project" element={<Project/>}/>
-      <Route path="/profileothers" element={<Profileothers/>}/>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/collaborations" element={<Collaboration />} />
+        <Route path="/profile/profileposts" element={<Profileposts />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/profileothers" element={<Profileothers />} />
 
-      <Route path="/team" element={<Team />}/>
-      <Route path="/projectsettings" element={<ProjectSettings/>}/>
-      <Route path="/registrationpage" element={<Registrationpage/>}/>
-      <Route path="/settings" element={<Settingsbar/>}/>
-      <Route path="/changepassword" element={<Cardthree/>}/>
-      <Route path="/blocked" element={<Cardfour/>}/>
-      <Route path="/delete" element={<Cardfive/>}/>
+        <Route path="/team" element={<Team />} />
+        <Route path="/projectsettings" element={<ProjectSettings />} />
+        <Route path="/registrationpage" element={<Registrationpage />} />
+        <Route path="/settings" element={<Settingsbar />} />
+        <Route path="/changepassword" element={<Cardthree />} />
+        <Route path="/blocked" element={<Cardfour />} />
+        <Route path="/delete" element={<Cardfive />} />
 
-    </Routes></>
+      </Routes>
+
+    </>
 
   );
 }
--- src/newcomponents/Topbar/Topbar.jsx
+++ src/newcomponents/Topbar/Topbar.jsx
@@ -1,21 +1,34 @@
 import "./Topbar.css";
-import {FaSearch,FaHome} from 'react-icons/fa'
-import {RiChatSmile2Fill} from  "react-icons/ri"
-import {BsFillPlusCircleFill} from "react-icons/bs"
+import { FaSearch, FaHome } from 'react-icons/fa'
+import { RiChatSmile2Fill } from "react-icons/ri"
+import { BsFillPlusCircleFill } from "react-icons/bs"
 import { Link } from "react-router-dom";
-import {FaBars} from 'react-icons/fa';
-import { useState,} from "react"
-import { MdExplore,MdOutlineNotificationsNone,MdSettings} from "react-icons/md"
+import { FaBars } from 'react-icons/fa';
+import { useState, } from "react"
+import { MdExplore, MdOutlineNotificationsNone, MdSettings } from "react-icons/md"
+import axios from "axios"
 
 import { IconContext } from 'react-icons';
-import {IoIosNotificationsOutline} from "react-icons/io";
+import { IoIosNotificationsOutline } from "react-icons/io";
 import Modal4 from "../Modal4/Modal4";
- function Topbar() {
+function Topbar() {
   const [openModal, setOpenModal] = useState(false);
+  const [searchField, setSearchField] = useState("")
+
+  const search = () => {
+    console.log("hello")
+    const request = {
+      'projectAdmin': String(getCookie('username'))
+    }
+    axios.get("http://localhost:8000/search/" + searchField, request).then((res) => {
+
+    })
+  }
+
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
-       <span className="logo"> <Link to="/home">Canary</Link></span>
+        <span className="logo"> <Link to="/home">Canary</Link></span>
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
@@ -23,28 +36,41 @@ import Modal4 from "../Modal4/Modal4";
           <input
             placeholder="Search for friend, post or video"
             className="searchInput"
+            value = {searchField}
+            onKeyDown={() => search()}
           />
         </div>
       </div>
       <div className="topbarRight">
-        
+
         <div className="topbarIcons">
-         
-        
-         
+
+
+
           <div className="topbarIconItem">
             <IoIosNotificationsOutline onClick={() =>
-          setOpenModal(true)}/>
+              setOpenModal(true)} />
             <span className="topbarIconBadge">1</span>
-          </div> 
+          </div>
           <div className="topbarIconItem">
-          <Link to="/profile"><img src="/images/avatar.png" alt="" className="topbarImg"/> </Link>
-          </div> 
-        </div> 
+            <Link to="/profile"><img src="/images/avatar.png" alt="" className="topbarImg" /> </Link>
+          </div>
         </div>
-        {openModal && <Modal4 closeModal={setOpenModal} />} 
       </div>
-  
+      {openModal && <Modal4 closeModal={setOpenModal} />}
+    </div>
+
   );
 }
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
 export default Topbar
