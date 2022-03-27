01dd3ed9fe86f0db6ae175fd5988ede395a985f6
na2007 <71809150+na2007@users.noreply.github.com>
2022-03-27 12:23:56 +0400

    completed moderatoraccount

---
+++ src/newcomponents/Moderator/Moderator.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import"./Moderator.css"
+function Moderator() {
+  return (
+    <div>
+    <body className="profilecc">
+    <div className="profile-container">
+        <div className="profileconc">
+      
+        <div className='profile-image-cropper'>
+          <img src="/images/profile.jpg" className="profile-image"/>
+        </div>
+
+        <h1 className="profile-user">Username</h1>
+        <button className='message-btn5'>Message</button>
+        </div>
+        </div>
+    </body>
+    </div>
+  )
+}
+
+export default Moderator
