e29cc9f2d63092224aedddfd5e5f85229499f685
Aazim Faiz <as2025@hw.ac.uk>
2022-03-29 07:40:13 +0400

    react-create-app on Heroku

--- src/JS_Files/websocket.js
+++ src/JS_Files/websocket.js
@@ -16,6 +16,7 @@ class WebSocketService {
     connect(setUpdater) {
       //const path = config.API_PATH;
       console.log('wheee')
+      console.log("made changed")
       this.socketRef = new WebSocket('wss://localhost:8000/ws/');
       this.socketRef.onopen = () => {
         console.log('WebSocket open');
